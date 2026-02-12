439e07b1d63a88eeb0fdd7399938eced97819243
Abdikadir-noor <abdikadir.noor@student.kyh.se>
2026-02-12 19:38:34 +0100

    Update CaesarCipherTests.cs

--- tests/EncryptionApi.Tests/CaesarCipherTests.cs
+++ tests/EncryptionApi.Tests/CaesarCipherTests.cs
@@ -2,15 +2,7 @@ using Xunit;
 
 public class CaesarCipherTests
 {
-    [Theory]
-    [InlineData("abc", 3, "def")]
-    [InlineData("XYZ", 3, "ABC")]
-    [InlineData("Hello, World!", 3, "Khoor, Zruog!")]
-    [InlineData("def", -3, "abc")]
-    public void Transform_ShiftsLetters(string input, int shift, string expected)
-    {
-        var result = CaesarCipher.Transform(input, shift);
-
+   
         Assert.Equal(expected, result);
     }
 
@@ -24,11 +16,5 @@ public class CaesarCipherTests
 
     [Fact]
     public void Decrypt_ReversesEncrypt()
-    {
-        var original = "Hej varlden!";
-        var encrypted = CaesarCipher.Transform(original, CaesarCipher.DefaultShift);
-        var decrypted = CaesarCipher.Transform(encrypted, -CaesarCipher.DefaultShift);
-
-        Assert.Equal(original, decrypted);
-    }
+   
 }
